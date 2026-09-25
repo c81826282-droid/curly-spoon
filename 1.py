f1ee92aa9dae7a733f2c107f995d10feca535f14
c81826282-droid <c81826282@gmail.com>
2026-09-25 18:18:08 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+#Import necessary libraries
+from tkinter import *
+
+#create window
+window = Tk()
+
+#set the window title and geometry
+window.title('DEMO WINDOW')
+window.geometry('400x300')
+
+#start the gui main loop
+window.mainloop()
